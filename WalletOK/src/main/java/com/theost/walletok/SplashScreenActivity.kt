a4ed97ac6a451fe005c19a161dc77afe2e44c175
korefu <korfucianist@gmail.com>
2021-08-17 14:41:33 +0300

    implement splash screen

---
+++ WalletOK/src/main/java/com/theost/walletok/SplashScreenActivity.kt
@@ -0,0 +1,14 @@
+package com.theost.walletok
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
+        finish()
+    }
+}
