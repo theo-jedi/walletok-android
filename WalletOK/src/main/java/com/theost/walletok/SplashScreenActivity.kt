854323fd82e0011749d743c5af6cd8804c563d15
Semviel <semviel22@gmail.com>
2021-08-17 15:07:26 +0300

    fixes

--- WalletOK/src/main/java/com/theost/walletok/SplashScreenActivity.kt
+++ WalletOK/src/main/java/com/theost/walletok/SplashScreenActivity.kt
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState);
         startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
         finish()
     }
