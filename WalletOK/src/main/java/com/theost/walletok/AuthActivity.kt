b96def8891334034c70ada953da3adf829e883de
fedor-jedi <fedor.contact@yandex.ru>
2021-08-17 22:17:32 +0300

    Implemented user authorization

--- WalletOK/src/main/java/com/theost/walletok/AuthActivity.kt
+++ WalletOK/src/main/java/com/theost/walletok/AuthActivity.kt
@@ -4,8 +4,29 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.widget.AppCompatButton
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+
 
 class AuthActivity : AppCompatActivity() {
+
+    private val authorizationHandler =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult? ->
+
+            if (result?.resultCode == RESULT_OK) {
+
+                val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                val account = task.result
+
+                signIn(account)
+            }
+
+        }
 
     companion object {
         fun newIntent(context: Context) : Intent {
@@ -16,5 +37,34 @@ class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
+
+        findViewById<AppCompatButton>(R.id.auth_button).setOnClickListener { authorizationHandler.launch(getSignInIntent()) }
     }
+
+    override fun onStart() {
+        super.onStart()
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+        signIn(account)
+    }
+
+    private fun getSignInIntent(): Intent {
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.server_client_id))
+            .requestEmail()
+            .build()
+
+        val mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        return mGoogleSignInClient.signInIntent
+    }
+
+    private fun signIn(account: GoogleSignInAccount?) {
+        if (account != null) {
+            // Toast for debug only
+            Toast.makeText(this, "You're authorized", Toast.LENGTH_SHORT).show()
+            // start next activity
+        }
+    }
+
 }
--- WalletOK/src/main/java/com/theost/walletok/SplashScreenActivity.kt
+++ WalletOK/src/main/java/com/theost/walletok/SplashScreenActivity.kt
@@ -1,13 +1,16 @@
 package com.theost.walletok
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState);
-        startActivity(MainActivity.newIntent(this))
+        super.onCreate(savedInstanceState)
+        val intent = AuthActivity.newIntent(this)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK and Intent.FLAG_ACTIVITY_NO_HISTORY
+        startActivity(intent)
         finish()
     }
 }
