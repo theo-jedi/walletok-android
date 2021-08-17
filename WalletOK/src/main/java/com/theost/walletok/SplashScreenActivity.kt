3e4d81a04ad4dec40a2dd96f40be3594dedae19e
Semviel <semviel22@gmail.com>
2021-08-17 16:43:19 +0300

    add companion object to main activity

--- WalletOK/src/main/java/com/theost/walletok/SplashScreenActivity.kt
+++ WalletOK/src/main/java/com/theost/walletok/SplashScreenActivity.kt
@@ -1,15 +1,13 @@
 package com.theost.walletok
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
-        startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
+        startActivity(MainActivity.newIntent(this))
         finish()
     }
 }
