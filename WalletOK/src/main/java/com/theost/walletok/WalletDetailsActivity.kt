f33e210438c05639bc7dd89f9de75f0537ee4450
Semviel <semviel22@gmail.com>
2021-08-17 23:31:33 +0300

    активность вместо фрагмента, сделан тулбар

---
+++ WalletOK/src/main/java/com/theost/walletok/WalletDetailsActivity.kt
@@ -0,0 +1,41 @@
+package com.theost.walletok
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.theost.walletok.databinding.ActivityWalletDetailsBinding
+
+class WalletDetailsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWalletDetailsBinding
+
+    companion object {
+        fun newIntent(context: Context): Intent {
+            return Intent(context, WalletDetailsActivity::class.java)
+        }
+    }
+
+    override fun onCreate(
+        savedInstanceState: Bundle?
+    ) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityWalletDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        binding.toolbar.setNavigationIcon(R.drawable.ic_arrow_back)
+        binding.toolbar.setNavigationOnClickListener {
+            finish()
+        }
+        binding.addOperationBtn.setOnClickListener {
+            Toast.makeText(this, "Нажата кнопка", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.options_wallet_details, menu)
+        return true
+    }
+}
