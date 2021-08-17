e17361064e6229d0235d952e241c7d2aaa5d7e96
Semviel <semviel22@gmail.com>
2021-08-17 21:31:29 +0300

    make wallet details content

--- WalletOK/src/main/java/com/theost/walletok/WalletDetailsFragment.kt
+++ WalletOK/src/main/java/com/theost/walletok/WalletDetailsFragment.kt
@@ -5,10 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.theost.walletok.databinding.FragmentHomeBinding
+import com.theost.walletok.databinding.FragmentWalletDetailsBinding
 
-class HomeFragment : Fragment() {
-    private var _binding: FragmentHomeBinding? = null
+class WalletDetailsFragment : Fragment() {
+    private var _binding: FragmentWalletDetailsBinding? = null
+
     // This property is only valid between onCreateView and onDestroyView.
     private val binding get() = _binding!!
     override fun onCreateView(
@@ -16,7 +17,7 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentWalletDetailsBinding.inflate(inflater, container, false)
         return binding.root
     }
 }
