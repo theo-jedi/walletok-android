e3ae6d28e3c2b37c2727bd0f80a4c14e482e5bbf
fedor-jedi <fedor.contact@yandex.ru>
2021-08-17 16:02:55 +0300

    Created Auth UI

---
+++ WalletOK/src/main/java/com/theost/walletok/AuthActivity.kt
@@ -0,0 +1,20 @@
+package com.theost.walletok
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AuthActivity : AppCompatActivity() {
+
+    companion object {
+        fun newIntent(context: Context) : Intent {
+            return Intent(context, MainActivity::class.java)
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
