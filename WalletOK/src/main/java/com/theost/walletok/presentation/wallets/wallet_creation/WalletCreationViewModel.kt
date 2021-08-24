bb301db6b7d467ad2820dfa1a7d238cf643f0cd1
Semviel <semviel22@gmail.com>
2021-08-24 23:31:42 +0300

    работа над созданием кошельков

---
+++ WalletOK/src/main/java/com/theost/walletok/presentation/wallets/wallet_creation/WalletCreationViewModel.kt
@@ -0,0 +1,8 @@
+package com.theost.walletok.presentation.wallets.wallet_creation
+
+import androidx.lifecycle.ViewModel
+import com.theost.walletok.data.models.WalletCreationModel
+
+class WalletCreationViewModel : ViewModel() {
+    var walletCreationModel = WalletCreationModel()
+}
